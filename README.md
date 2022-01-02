7243c5edc63208ef3f3b7b819132b25aca3f3ce6
Arya Krishna <dr.aryakris@outlook.com>
2022-01-01 17:08:35 -0800

    practicing process.argv

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Node-Practice
 practicing node js commands 
+
+# process.argv() Method in Node.js
+The process.argv() method is used for returning all the command-line arguments that were passed when the Node.js process was being launched.
+
+# Results
+![alt text](./Images/process-argv.png)
--- argv.js
+++ argv.js
@@ -1,3 +1,5 @@
+// Method1
+
 // Node.js program to demonstrate the use of process.argv
 
 // Importing the process module
@@ -5,3 +7,17 @@ const process = require('process');
 
 // Printing property value for process.argv
 console.log(process.argv);
+// Method 2
+
+// Node.js program to demonstrate the use of process.argv
+
+// Importing the process module
+const process = require('process');
+
+// Printing process.argv property value
+var args = process.argv;
+
+console.log("Total number of arguments are: " + args.length);
+args.forEach((val, index) => {
+    console.log(`${index}: ${val}`);
+});
