bc8055c00f302fe8880a912519fb90d3fb1cf0dc
Arya Krishna <dr.aryakris@outlook.com>
2022-01-01 16:12:03 -0800

    argv process

---
+++ argv.js
@@ -0,0 +1,7 @@
+// Node.js program to demonstrate the use of process.argv
+
+// Importing the process module
+const process = require('process');
+
+// Printing property value for process.argv
+console.log(process.argv);
